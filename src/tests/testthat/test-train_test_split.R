5d5c854676db7712cf9414d67bf2c3d19ef0bc15
AniswagMC <anirudh.duggal.ad.ad@gmail.com>
2023-03-12 16:32:11 -0700

    All the testthat tests in place

---
+++ src/tests/testthat/test-train_test_split.R
@@ -0,0 +1,16 @@
+library(testthat)
+source("../src/R/train_test_split.R")
+source("../src/tests/testthat/helper_train_test_split.R")
+
+test_that("train_test_split should ask the user to input a valid integer", {
+  expect_error(splittingData(test_df, 1.1, "rating"))
+  expect_error(splittingData(test_df, 0, "rating"))
+})
+
+test_that("train_test_split should ask the user to enter a valid column name present within the data frame", {
+  expect_error(splittingData(test_df, 0.5, "name"))
+})
+
+test_that("train_test_split should return two data frames, one for testing and one for training", {
+  expect_type(load_data(test_url), "list")
+})
