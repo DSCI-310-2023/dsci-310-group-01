3dd0a7c411aec8c4fca72b5f829b1e0c28cb2d8f
Ani D <51113564+AniswagMC@users.noreply.github.com>
2023-03-12 16:46:22 -0700

    Update test-train_test_split.R

--- src/tests/testthat/test-train_test_split.R
+++ src/tests/testthat/test-train_test_split.R
@@ -1,16 +1,16 @@
 library(testthat)
 source("../src/R/train_test_split.R")
-source("../src/tests/testthat/helper_train_test_split.R")
+source("../src/tests/testthat/helper--train_test_split.R")
 
 test_that("train_test_split should ask the user to input a valid integer", {
-  expect_error(splittingData(test_df, 1.1, "rating"))
-  expect_error(splittingData(test_df, 0, "rating"))
+  expect_error(train_test_split(test_df, 1.1, "rating"))
+  expect_error(train_test_split(test_df, 0, "rating"))
 })
 
 test_that("train_test_split should ask the user to enter a valid column name present within the data frame", {
-  expect_error(splittingData(test_df, 0.5, "name"))
+  expect_error(train_test_split(test_df, 0.5, "name"))
 })
 
 test_that("train_test_split should return two data frames, one for testing and one for training", {
-  expect_type(splittingData(test_df, 0.5, "genre"), "list")
+  expect_type(train_test_split(test_df, 0.5, "genre"), "list")
 })
