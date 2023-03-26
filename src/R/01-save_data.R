f2e4fefa8d8f1b48e38c3805b252ef1211dcd809
Justin Wong <78821953+jw327@users.noreply.github.com>
2023-03-25 22:20:13 -0700

    Script 1 working

--- src/R/01-save_data.R
+++ src/R/01-save_data.R
@@ -1,23 +1,32 @@
-#author: Longfei Guan
+#author: Longfei Guan, Justin Wong
 #date: 2022-03-24
 
-"This scripts reads a dataset from the given URL and saves it as a csv file named 'data.csv'.
+#' This script reads a dataset from the given URL and saves it as a csv file named 'garments_worker_productivity.csv'
 
-Usage: src/R/save_data.R <file_path> <out_dir>
-" -> doc
+doc<-"
+Usage:
+  src/R/01-write_data.R --url=<url>  --out_dir=<output_dir> 
+    Options:
+    --url=<url>		
+      --out_dir=<output_dir>		
+        "
 
-source("/load_data.R")
 suppressPackageStartupMessages({
   library(tidyverse)
   library(broom)
   library(GGally)
   library(leaps)
   library(glmnet)
+  library(here)
   library(docopt)
 })
 
+source(here("src/R/load_data.R"))
+
 opt <- docopt(doc)
+main <- function(url, out_dir) {
+  data <- load_data(opt$url)
+  write_csv(data, paste0(out_dir, "/garments_worker_productivity.csv"))
+}
 
-data<-load_data(doc$file_path)
-
-write_csv(data, paste0(opt$out_dir, "/data.csv"))
+main(opt[["--url"]], opt[["--out_dir"]])
