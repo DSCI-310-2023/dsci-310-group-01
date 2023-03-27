17a1515c7d3bff3c5ed701b04434dfd6a7ed79d4
Justin Wong <78821953+jw327@users.noreply.github.com>
2023-03-26 17:29:12 -0700

    Fixed typo in test

--- src/tests/testthat/test-load_data.R
+++ src/tests/testthat/test-load_data.R
@@ -7,5 +7,4 @@ test_that("`load_data` should report an error when a string of a URL is not inpu
 
 test_that("`load_data` should return a data frame", {
   expect_s3_class(load_data(test_url), "data.frame")
-  
 })
